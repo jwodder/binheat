c031260756fb8db15037d4c9650a0a6c090ca94f
John T. Wodder II <git@varonathe.org>
2019-04-14 21:09:31 +0000

    OO rewrite

--- binheat.py
+++ binheat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from   collections               import defaultdict
 import re
 import click
 from   reportlab.pdfbase         import pdfmetrics
@@ -9,6 +8,168 @@ from   reportlab.pdfgen.canvas   import Canvas
 EM = 0.7  # expected em-to-font-size ratio
 PADDING = 5
 LINEPAD = 2.5
+
+COL_BG_COLOR = (0.8, 0.8, 0.8)
+ROW_BG_COLOR = (1, 1, 0.5)
+
+class BinHeat:
+    def __init__(self, allow_extra=False):
+        #: Whether to allow (True) or discard (False) pairs containing an
+        #: unknown label.  Only has an effect when
+        #: `set_row_labels()`/`set_column_labels()` has been called.
+        self.allow_extra = allow_extra
+        self.row_labels_set = False
+        self.column_labels_set = False
+        self.rows2indices = {}
+        self.columns2indices = {}
+        self.pairs = set()
+
+    def set_row_labels(self, labels):
+        if self.row_labels_set:
+            raise RuntimeError('set_row_labels() called more than once')
+        elif self.pairs:
+            raise RuntimeError('set_row_labels() called after add_pair()')
+        else:
+            self.row_labels_set = True
+        self.rows2indices = {l:i for i,l in enumerate(labels)}
+
+    def set_column_labels(self, labels):
+        if self.column_labels_set:
+            raise RuntimeError('set_column_labels() called more than once')
+        elif self.pairs:
+            raise RuntimeError('set_column_labels() called after add_pair()')
+        else:
+            self.column_labels_set = True
+        self.columns2indices = {l:i for i,l in enumerate(labels)}
+
+    def add_pair(self, row, column):
+        if not self.allow_extra:
+            if self.row_labels_set and row not in self.rows2indices:
+                return
+            if self.column_labels_set and column not in self.columns2indices:
+                return
+        if row not in self.rows2indices:
+            self.rows2indices[row] = len(self.rows2indices)
+        if column not in self.columns2indices:
+            self.columns2indices[column] = len(self.columns2indices)
+        self.pairs.add((row, column))
+
+    @property
+    def rows(self):
+        """ The number of distinct row labels in the binary heat map """
+        return len(self.rows2indices)
+
+    @property
+    def columns(self):
+        """ The number of distinct column labels in the binary heat map """
+        return len(self.columns2indices)
+
+    @property
+    def row_labels(self):
+        return sorted(
+            self.rows2indices.keys(),
+            key=self.rows2indices.__getitem__,
+        )
+
+    @property
+    def column_labels(self):
+        return sorted(
+            self.columns2indices.keys(),
+            key=self.columns2indices.__getitem__,
+        )
+
+    def get_indexed_pairs(self):
+        for r,c in self.pairs:
+            yield (self.rows2indices[r], self.columns2indices[c])
+
+    def sort_labels(self):
+        ### TODO: Take allow_extra into account when *_labels_set
+        if not self.row_labels_set:
+            self.rows2indices = {
+                k:i for i,k in enumerate(sorted(self.rows2indices.keys()))
+            }
+        if not self.column_labels_set:
+            self.columns2indices = {
+                k:i for i,k in enumerate(sorted(self.columns2indices.keys()))
+            }
+
+    def render(self, outfile, font_name, font_size):
+        ### TODO: Use canvas.stringWidth() here instead:
+        leftlen = max(map(len, self.row_labels)) * EM * font_size
+        toplen  = max(map(len, self.column_labels)) * EM * font_size
+
+        miny = self.rows * font_size * 1.2
+        maxx = self.columns * font_size * 1.2
+
+        c = Canvas(
+            outfile,
+            (leftlen + maxx + PADDING * 2, miny + toplen + PADDING * 2),
+        )
+        # Set coordinates so that LL corner has coord (-leftlen-PADDING,
+        # -miny-PADDING) and the origin is at the point where the borders of the
+        # row & column labels meet:
+        c.translate(leftlen+PADDING, miny+PADDING)
+
+        c.setFont(font_name, font_size)
+
+        lineheight = font_size * 1.2
+        radius = lineheight / 3
+
+        c.setFillColorRGB(*COL_BG_COLOR)
+        for i in range(0, self.columns, 2):
+            c.rect(
+                i * lineheight,
+                -miny,
+                lineheight,
+                miny + toplen,
+                stroke=0,
+                fill=1,
+            )
+
+        c.setFillColorRGB(*ROW_BG_COLOR)
+        for i in range(2, self.rows+1, 2):
+            # Yes, it starts at 2, so that the positive rectangle height will make
+            # it fill row 1.
+            c.rect(
+                -leftlen,
+                -i * lineheight,
+                leftlen + maxx,
+                lineheight,
+                stroke=0,
+                fill=1,
+            )
+
+        c.setFillColorRGB(0, 0, 0)
+        c.line(0, toplen, 0, -miny)
+        c.line(-leftlen, 0, maxx, 0)
+
+        for i, label in enumerate(self.row_labels):
+            ### TODO: Replace with drawRightString()?
+            c.drawString(
+                -c.stringWidth(label) - LINEPAD,
+                -(i+1) * lineheight + font_size / 3,
+                label,
+            )
+
+        for i, label in enumerate(self.column_labels):
+            c.saveState()
+            c.translate((i+1) * lineheight, 0)
+            c.rotate(90)
+            c.drawString(LINEPAD, font_size / 3, label)
+            c.restoreState()
+
+        for row, col in self.get_indexed_pairs():
+            c.circle(
+                (col+0.5) * lineheight,
+                -(row+0.5) * lineheight,
+                radius,
+                stroke=0,
+                fill=1,
+            )
+
+        c.showPage()
+        c.save()
+
 
 @click.command(context_settings={"help_option_names": ["-h", "--help"]})
 @click.option('-F', '--font', metavar='TTF_FILE',
@@ -32,142 +193,38 @@ def main(infile, outfile, font, font_size, transpose, multiline, no_sort,
     else:
         font_name = 'Times-Roman'
 
-    matrix = defaultdict(dict)
-
-    def pushPair(left, top):
-        nonlocal rows, cols
-        if transpose:
-            left, top = top, left
-        matrix[left][top] = 1
-        if no_sort and not left_labels and left not in lefts:
-            lefts[left] = rows
-            rows += 1
-        if no_sort and not top_labels and top not in tops:
-            tops[top] = cols
-            cols += 1
+    bh = BinHeat()
 
     if transpose:
         left_labels, top_labels = top_labels, left_labels
+    if left_labels:
+        bh.set_row_labels(strip_read(left_labels))
+    if top_labels:
+        bh.set_column_labels(strip_read(top_labels))
 
-    lefts = readList(left_labels) if left_labels else {}
-    rows = len(lefts)
-
-    tops = readList(top_labels) if top_labels else {}
-    cols = len(tops)
-
-    for line in infile:
-        line = line.rstrip()
-        if line == '' or line.lstrip().startswith('#'):
-            continue
+    for line in strip_read(infile):
         left, *top = re.split(r'\t+', line)
-        if multiline:
-            for t in top:
-                pushPair(left, t)
-        elif top:
-            pushPair(left, top[0])
+        for t in (top if multiline else top[:1]):
+            if transpose:
+                bh.add_pair(t, left)
+            else:
+                bh.add_pair(left, t)
 
     if not no_sort:
-        if not left_labels:
-            lefts = {k:i for i,k in enumerate(sorted(matrix.keys()))}
-            rows = len(lefts)
-        if not top_labels:
-            tops_set = set()
-            for v in matrix.values():
-                tops_set.update(v.keys())
-            tops = {k:i for i,k in enumerate(sorted(tops_set))}
-            cols = len(tops)
+        bh.sort_labels()
 
-    ### TODO: Use canvas.stringWidth() here instead:
-    leftlen = max(map(len, lefts.keys())) * EM * font_size
-    toplen  = max(map(len, tops.keys()))  * EM * font_size
+    bh.render(outfile, font_name, font_size)
 
-    miny = rows * font_size * 1.2
-    maxx = cols * font_size * 1.2
-
-    c = Canvas(
-        outfile,
-        (leftlen + maxx + PADDING * 2, miny + toplen + PADDING * 2),
-    )
-    # Set coordinates so that LL corner has coord (-leftlen-PADDING,
-    # -miny-PADDING):
-    c.translate(leftlen+PADDING, miny+PADDING)
-
-    c.setFont(font_name, font_size)
-
-    lineheight = font_size * 1.2
-    radius = lineheight / 3
-
-    def dot(canvas, col, row):
-        canvas.circle(
-            (col+0.5) * lineheight,
-            -(row+0.5) * lineheight,
-            radius,
-            stroke=0,
-            fill=1,
-        )
-
-    c.setFillColorRGB(0.8, 0.8, 0.8)
-    for i in range(0, cols, 2):
-        c.rect(
-            i * lineheight,
-            -miny,
-            lineheight,
-            miny + toplen,
-            stroke=0,
-            fill=1,
-        )
-
-    c.setFillColorRGB(1, 1, 0.5)
-    for i in range(2, rows+1, 2):
-        # Yes, it starts at 2, so that the positive rectangle height will make
-        # it fill row 1.
-        c.rect(
-            -leftlen,
-            -i * lineheight,
-            leftlen + maxx,
-            lineheight,
-            stroke=0,
-            fill=1,
-        )
-
-    c.setFillColorRGB(0, 0, 0)
-    c.line(0, toplen, 0, -miny)
-    c.line(-leftlen, 0, maxx, 0)
-
-    for k,v in lefts.items():
-        ### TODO: Replace with drawRightString()?
-        c.drawString(
-            -c.stringWidth(k) - LINEPAD,
-            -(v+1) * lineheight + font_size / 3,
-            k,
-        )
-
-    for k,v in tops.items():
-        c.saveState()
-        c.translate((v+1) * lineheight, 0)
-        c.rotate(90)
-        c.drawString(LINEPAD, font_size / 3, k)
-        c.restoreState()
-
-    for k,v in matrix.items():
-        if k in lefts:
-            for j in v.keys():
-                if j in tops:
-                    dot(c, tops[j], lefts[k])
-
-    c.showPage()
-    c.save()
-
-def readList(fp):
-    i = 0
-    lines2nums = {}
+def strip_read(fp):
+    r"""
+    Yield the lines in file ``fp`` with trailing whitespace stripped and with
+    lines beginning with ``/^\s*#/`` (i.e., comment lines) discarded
+    """
     for line in fp:
         line = line.rstrip()
         if line == '' or line.lstrip().startswith('#'):
             continue
-        lines2nums[line] = i
-        i += 1
-    return lines2nums
+        yield line
 
 if __name__ == '__main__':
     main()
